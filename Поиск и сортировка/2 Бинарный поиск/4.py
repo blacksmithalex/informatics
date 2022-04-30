98f087ad2c171b97fc75f8966c6d8f1b46bac0cb
blacksmithalex <blacksmithalex@mail.ru>
2022-04-30 15:49:29 +0300

    serch tasks

--- Поиск и сортировка/2 Бинарный поиск/4.py
+++ Поиск и сортировка/2 Бинарный поиск/4.py
@@ -9,25 +9,12 @@ def lbs(a, x):
             r = c
     return r
 
-def rbs(a, x):
-    l = 0
-    r = len(a)
-    while l + 1 != r:
-        c = (l + r) // 2
-        if a[c] > x:
-            r = c
-        else:
-            l = c
-    return l
-
 N, K = [int(x) for x in input().split()]
 a = [int(x) for x in input().split()]
 f = [int(x) for x in input().split()]
 
 for x in f:
-    indl, indr = lbs(a, x), rbs(a, x)
-    var1, var2 = a[indl], a[indr]
-    if var1 - x < x - var2:
-        print(var1)
+    if a[lbs(a, x)] == x:
+        print('YES')
     else:
-        print(var2)
+        print('NO')
