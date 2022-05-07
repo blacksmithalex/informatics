c9f1bc2c41cf6684aa1ef150b3dcfee1fe298ca2
blacksmithalex <blacksmithalex@mail.ru>
2022-05-07 16:00:05 +0300

    new tasks

--- Поиск и сортировка/2 Бинарный поиск/894.py
+++ Поиск и сортировка/2 Бинарный поиск/894.py
@@ -1,44 +1,35 @@
 from math import ceil
-def Time(v, d, times, dist):
-    if times[0] > dist[0] / v:
-        return -1
-    else:
-        T = dist[0] / v
-        for i in range(1, len(times)):
-            ti = times[i] - times[i - 1]
-            disti = dist[i] - dist[i - 1]
-            if ti > disti / v:
-                return -1
-            T += disti / v
-        return 2 * T + len(times) * d
-def update1(time):
-    h, m = [int(x) for x in time.split(':')]
-    return h * 60 + m
-def update2(time):
-    time = ceil(time)
-    time %= 1440
-    h, m = time // 60,  time % 60
-    return '0' * (2 - len(str(h))) + str(h) + ':' + '0' * (2 - len(str(m))) + str(m)
-
 vmax, d = [int(x) for x in input().split()]
-N = int(input())
-dist, times = [], []
-for _ in range(N):
-    disti, timei = input().split()
-    dist.append(int(disti))
-    times.append(update1(timei))
+count = int(input())
 
-l = 1
-r = vmax + 1
-while l + 1 != r:
-    v = (l + r) // 2
-    t = Time(v, d, times, dist)
-    if t != -1:
-        l = v
+distance, growthTime, previousX  = [], [], 0
+
+for _ in range(count):
+    X, H, M = [int(x) for x in input().replace(":", " ").split()]
+    distance.append(X - previousX)
+    previousX = X
+    growthTime.append(H * 60 + M)
+lastX = previousX
+
+left = 0
+right = growthTime[-1]
+for j in range(100):
+    middle = (right + left) / 2
+    time = middle
+    for i in range(count-1):
+        time += distance[i]/vmax
+        if time >= growthTime[i]:
+            time += d
+    time += distance[-1]/vmax
+    if time == growthTime[-1]:
+        break
+    elif time < growthTime[-1]:
+        left = middle
     else:
-        r = v
+        right = middle
 
-res = Time(2, d, times, dist)
-print(update2(res))
+time = ceil(2 * lastX / vmax + count * d + right)
+H = time // 60
+M = time % 60
 
-
+print(str(H).zfill(2) + ":" + str(M).zfill(2))
